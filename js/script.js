a985457d499df9429c89fa1b040931f99d20efc5
developer <developer@email.com>
2021-01-19 18:55:17 +0100

    init commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+// Code Scroll Hash 800 ms
+
+$(document).ready(function(){
+  // $("a.scroll").on('click', function(event){
+
+  //     var hash = this.hash;
+
+  //     $('html,body').animate({scrollTop: $(hash).offset().top},800,function(){})
+  // });
+
+  // Activate lib AOS Animate
+    AOS.init();
+ 
+});
+
+
